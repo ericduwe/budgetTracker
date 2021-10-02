48e58f08ae8b06f747f9935355cefb4398a58457
Eric Duwe <ericduwe@gmail.com>
2021-10-01 23:29:30 -0500

    fixed service worker and added service-worker script to html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,11 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/topic.html',
-    '/style.css',
+    '/styles.css',
     '/index.js',
     '/indexedDb.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
     '/manifest.webmanifest'
     
 
